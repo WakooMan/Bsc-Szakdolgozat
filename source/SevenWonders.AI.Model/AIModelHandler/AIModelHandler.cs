5cb591457cfcbc8f9ed8b4e19d2c0e3d56102f81
Viktor Magyar <magyar.viktor.2000@gmail.com>
2026-04-25 20:33:20 +0200

    Fixes

--- source/SevenWonders.AI.Model/AIModelHandler/AIModelHandler.cs
+++ source/SevenWonders.AI.Model/AIModelHandler/AIModelHandler.cs
@@ -14,7 +14,8 @@ namespace SevenWonders.AI.Model.AIModelHandler
             m_session = null;
             m_aiModels = new Dictionary<AIModelType, string>
             {
-                { AIModelType.Easy, "seven_wonders_easy_agent.onnx" }
+                { AIModelType.Easy, "seven_wonders_easy_agent.onnx" },
+                { AIModelType.Medium, "seven_wonders_medium_agent.onnx" }
             };
         }
 
